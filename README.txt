4327120bf3efa924ed9d218b72ff89c54a235326
moshbo <44542947+moshbo@users.noreply.github.com>
2022-01-11 19:19:06 +0200

    1.0.1
    Update test

--- README.txt
+++ README.txt
@@ -8,7 +8,7 @@ Stable tag: 4.3
 Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
 
 
-~Current Version:1.0.0~
+~Current Version:1.0.1~
 
 == Description ==
 
@@ -19,5 +19,8 @@ Description
 Installation text
 
 == Changelog ==
+1.0.1
+Update test
+
 1.0.0
 Repo INIT
--- ctm-reg.php
+++ ctm-reg.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://www.valleydevelopers.com/
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Ctm_Reg
  *
  * @wordpress-plugin
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CTM_REG_VERSION', '1.0.0' );
+define( 'CTM_REG_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
